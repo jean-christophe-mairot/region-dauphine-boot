4d293552b61363819ebe895115bcc2eaaf7b9dd6
jc <jcmairot@gmail.com>
2020-10-25 18:05:45 +0100

    ajout js insertion de card en fonction d'un tableau dans global.js

--- assets/global.js
+++ assets/global.js
@@ -1,28 +1,43 @@
 const tabAvis = [
   {
-    avisImg: "/img/station.jpg",
+    avisImg: "img/station.jpg",
     title: "Title",
     text:
       "Lorem ipsum dolor sit amet consectetur adipisicing elit. Voluptate labore maxime dolore quaerat delectus. Nihil, fugiat alias eius id minus ad, officia qui eos quod incidunt ea blanditiis pariatur eum.",
   },
   {
-    avisImg: "/img/station.jpg",
+    avisImg: "img/station.jpg",
     title: "Title",
     text:
       "Lorem ipsum dolor sit amet consectetur adipisicing elit. Voluptate labore maxime dolore quaerat delectus. Nihil, fugiat alias eius id minus ad, officia qui eos quod incidunt ea blanditiis pariatur eum.",
   },
   {
-    avisImg: "/img/station.jpg",
+    avisImg: "img/station.jpg",
     title: "Title",
     text:
       "Lorem ipsum dolor sit amet consectetur adipisicing elit. Voluptate labore maxime dolore quaerat delectus. Nihil, fugiat alias eius id minus ad, officia qui eos quod incidunt ea blanditiis pariatur eum.",
   },
 ];
 
-console.log(tabAvis);
+// console.log(tabAvis);
 
 // const elemAvisImg = document.getElementById("img-cardAvis");
 // elemAvisImg.src = "/img/station.jpg";
-for (i = 0; i < tabAvis.length; i++) {
-  document.write(tabAvis[i]);
-}
+tabAvis.forEach((avis) => {
+  document.querySelector("div.avis").innerHTML +=
+    // card compléte
+
+    '<div class="card" style="width: 18rem">' +
+    '<img id="img-cardAvis" src="' +
+    avis.avisImg +
+    '" class="card-img-top" alt="..." />' +
+    '<div class="card-body">' +
+    '<h2 id="avisH2">' +
+    avis.title +
+    "</h2>" +
+    '<p class="card-text">' +
+    avis.text +
+    "</p>" +
+    "</div>" +
+    "</div>";
+});
